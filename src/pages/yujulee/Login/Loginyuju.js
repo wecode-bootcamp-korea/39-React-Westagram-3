fab64a39867c7ebb3d36f9cceee14735821f399b
yuju <yuju@iyujuui-MacBookPro.local>
2022-11-03 19:26:54 +0900

    ADD: 2차 수정 완료

--- src/pages/yujulee/Login/Loginyuju.js
+++ src/pages/yujulee/Login/Loginyuju.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import './Login.css';
+import React, { useState } from 'react';
+import './Loginyuju.scss';
 
-function Loginyuju(){
-    return (
-        <main>
-            <h1>Westagram</h1>
-            <div className="typeid">
-                <input type="text" className="idinput" placeholder="전화번호, 사용자 이름 또는 이메일" /> 
-            </div>
-            <div className="typepw">
-                <input type="password" className="pwinput" placeholder="비밀번호" />
-            </div>
-            <div className="login">
-                <button className="loginbtn" disabled>로그인</button>
-            </div>
-            <div className="forget">비밀번호를 잊으셨나요?</div>
-        </main>
-        );
-    }
+
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <main>
+      <h1 className="title">Westagram</h1>
+      <div className="typeid">
+        <input
+          type="text"
+          className="idinput"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+      </div>
+      <div className="typepw">
+        <input type="password" className="pwinput" placeholder="비밀번호" />
+      </div>
+      <div className="login">
+        <button onClick={() => {}} className="loginbtn" disabled>
+          로그인
+        </button>
+      </div>
+      <div className="forget">비밀번호를 잊으셨나요?</div>
+    </main>
+  );
+}
 export default Loginyuju;
--- src/pages/yujulee/Main/Mainyuju.js
+++ src/pages/yujulee/Main/Mainyuju.js
@@ -1,18 +1,142 @@
 import React from 'react';
-import Nav from './components/Nav';
-import Section from './components/Section';
-import Article from './components/Article';
-import Aside from './components/Aside';
-import './Main.css'
+import './Mainyuju.scss';
 
-function Mainyuju(){
-    return(
-        <>
-        <Nav />
-        <Section />
-        <Article />
-        <Aside />
-        </>
-    )
+function Story({ src, figcaption }) {
+  return (
+    <div className="story">
+      <img className="storyimg" src={src} width="100%" alt="story" />
+      <figcaption>{figcaption}</figcaption>
+    </div>
+  );
+}
+
+function Recommend({ src, id, friends }) {
+  return (
+    <div className="feedname">
+      <img className="preview" src={src} alt="story" />
+      <span>{id}</span>
+      <span className="friends">{friends}</span>
+      <span className="follow">팔로우</span>
+    </div>
+  );
+}
+
+function Mainyuju() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="icons">
+          <i className="fa-brands fa-instagram" />
+          <br />
+          <i className="fa-sharp fa-solid fa-house" />
+          <br />
+          <i className="fa-solid fa-magnifying-glass" />
+          <br />
+          <i className="fa-regular fa-paper-plane" />
+          <br />
+          <i className="fa-regular fa-heart" />
+          <br />
+          <i className="fa-regular fa-square-plus" />
+        </div>
+      </nav>
+      <div className="wrapper">
+        <main className="mainbox">
+          <section className="storybox">
+            <Story src="/images/img.yujulee/photo7.jpg" figcaption="_stagram" />
+            <Story src="/images/img.yujulee/photo6.jpg" figcaption="absdefg" />
+            <Story src="/images/img.yujulee/photo2.jpeg" figcaption="howru" />
+            <Story src="/images/img.yujulee/photo3.jpeg" figcaption="wewe_we" />
+            <Story
+              src="/images/img.yujulee/photo8.jpeg"
+              figcaption="soojinee"
+            />
+          </section>
+
+          <article className="feedarticle">
+            <div className="feedname">
+              <img
+                className="preview"
+                src="/images/img.yujulee/photo6.jpg"
+                width="100%"
+                alt="story"
+              />
+              <span>yuzu_like</span>
+              <span className="friends">대한민국</span>
+              <i className="fa-solid fa-ellipsis" />
+            </div>
+            <img
+              className="feedimg"
+              src="/images/img.yujulee/photo8.jpeg"
+              width="100%"
+              alt="story"
+            />
+            <br />
+            <div className="feedtalk">
+              <i className="fa-regular fa-heart press" />
+              <i className="fa-regular fa-comment" />
+              <i className="fa-regular fa-paper-plane" />
+              <br />
+              <div>좋아요 25개</div>
+              <div> yuzu_like 놀러가구싶다!!</div>
+              <div className="date">1일 전</div>
+              <ul className="writes">
+                <li className="write" />
+              </ul>
+            </div>
+            <div className="comment">
+              <i className="smileface fa-regular fa-face-smile" />
+              <input
+                type="text"
+                className="commentbox"
+                placeholder="댓글 달기..."
+              />
+              <button className="inputcomment" disable>
+                게시
+              </button>
+            </div>
+          </article>
+        </main>
+        <aside className="sidebar">
+          <div className="feedname">
+            <img
+              className="profile"
+              src="/images/img.yujulee/photo6.jpg"
+              alt="story"
+            />
+            <span>yuzu_like</span>
+            <span className="friends">이유주</span>
+            <span className="follow">전환</span>
+          </div>
+          <span className="all">회원님을 위한 추천</span>
+          <span className="viewall">모두 보기</span>
+          <Recommend
+            src="/images/img.yujulee/photo3.jpeg"
+            id="who_who"
+            friends="oh_vet94님, hongjye__님 외 11명이 팔로우"
+          />
+          <Recommend
+            src="/images/img.yujulee/photo6.jpg"
+            id="wecode_bootcamp"
+            friends="wylee0930님, oh_vet94님 외 13명이 팔로"
+          />
+          <Recommend
+            src="/images/img.yujulee/photo7.jpg"
+            id="sunnny_day"
+            friends="wylee0930님, oh_vet94님 외 13명이 팔로"
+          />
+          <Recommend
+            src="/images/img.yujulee/photo8.jpeg"
+            id="forever_for"
+            friends="s0.jeon님과 yeah915님이 팔로우합니다"
+          />
+          <Recommend
+            src="/images/img.yujulee/photo2.jpeg"
+            id="i_likeeee"
+            friends="회원님을 위한 추천"
+          />
+        </aside>
+      </div>
+    </>
+  );
 }
 export default Mainyuju;
