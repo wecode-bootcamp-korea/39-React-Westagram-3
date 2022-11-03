9a2ffaa64d72f63109fdf36852c747001c9664f8
kimboyoon <kimboyoon0908@gmail.com>
2022-11-03 11:21:44 +0900

    Add: Login, Main 페이지 옮기기

--- src/pages/boyoonkim/Login/Loginboyoon.js
+++ src/pages/boyoonkim/Login/Loginboyoon.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import './Loginboyoon.scss';
+import { useNavigate } from 'react-router-dom';
 
-function Loginboyoon() {
-  return <p>Hello world</p>;
-}
+const Loginboyoon = () => {
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('../main-boyoon');
+  };
+
+  return (
+    <div className="loginPage">
+      <header className="loginPageContent">
+        <div className="logo">Westagram</div>
+
+        <div className="inputBoxes">
+          <input
+            className="inputId"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input className="inputPw" type="password" placeholder="비밀번호" />
+          <button className="loginBtn" onClick={goToMain} /*disabled*/>
+            로그인
+          </button>
+        </div>
+
+        <div className="findPassword">
+          <p href="#">비밀번호를 잊으셨나요?</p>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default Loginboyoon;
--- src/pages/boyoonkim/Main/Mainboyoon.js
+++ src/pages/boyoonkim/Main/Mainboyoon.js
@@ -1,7 +1,265 @@
 import React from 'react';
+import './Mainboyoon.scss';
 
-function Mainboyoon() {
-  return <p>Hello world</p>;
+function Main() {
+  return (
+    <div className="mainPage">
+      <nav className="navBar">
+        <div className="navBar_logo">
+          <i className="fa-brands fa-instagram" />
+          <div>|</div>
+          <div className="navBar_logoInstagram">Westagram</div>
+        </div>
+
+        <div className="navBar_searchBar">
+          <i className="fa-solid fa-magnifying-glass" />
+          <input className="navBar_search" type="text" placeholder="검색" />
+        </div>
+
+        <div className="navBar_icons">
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+            alt=""
+          />
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+            alt=""
+          />
+          <img
+            src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
+            alt=""
+          />
+        </div>
+      </nav>
+
+      <main className="main_feeds">
+        <article className="main_article">
+          <div className="article_navbar">
+            <img
+              className="smallImage"
+              src="/images/img.boyoonkim/smile img.jpg"
+              alt=""
+            />
+            <div className="feedHeader">
+              <div className="id">wecode_bootcamp</div>
+              <div className="name">WeCode - 위코드</div>
+            </div>
+            <i className="fa-solid fa-ellipsis" />
+          </div>
+
+          <img
+            className="article_img"
+            src="/images/img.boyoonkim/dogimg.jpg"
+            alt=""
+          />
+
+          <div className="article_icons">
+            <div>
+              <i className="fa-solid fa-heart" />
+              <i className="fa-regular fa-comment" />
+              <i className="fa-solid fa-arrow-up-from-bracket" />
+            </div>
+
+            <div>
+              <i className="fa-regular fa-bookmark" />
+            </div>
+          </div>
+
+          <div className="article_content">
+            <img
+              className="content_Image"
+              src="/images/img.boyoonkim/boimg.jpg"
+              alt=""
+            />
+            <span className="content_likes">
+              <span className="content_likesName">booooni.k</span>님 외 777명이
+              좋아합니다.
+            </span>
+          </div>
+
+          <ul className="article_reply">
+            <li>
+              <span className="replyId">wecode_bootcamp</span>
+              <span>귀여운 웰시코기</span>
+            </li>
+          </ul>
+
+          <div className="article_replyBottom">
+            <input className="article_typingReply" placeholder="댓글 달기..." />
+            <button className="article_replyPush" disabled>
+              게시
+            </button>
+          </div>
+        </article>
+
+        <div className="main_right">
+          <div className="right_info">
+            <img
+              className="right_smallImage"
+              src="/images/img.boyoonkim/boimg.jpg"
+              alt=""
+            />
+            <div className="right_IdName">
+              <div className="id">booooni.k</div>
+              <div className="name">보윤</div>
+            </div>
+          </div>
+
+          <div className="right_storyFriends">
+            <div className="right_nav">
+              <div>스토리</div>
+              <div>모두보기</div>
+            </div>
+
+            <div className="right_story">
+              <div className="storyAround">
+                <img
+                  className="storyImg"
+                  src="/images/img.boyoonkim/dogimg2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="story_IdTime">
+                <div>sayho_oooo</div>
+                <div>20분 전</div>
+              </div>
+            </div>
+
+            <div className="right_story">
+              <div className="storyAround">
+                <img
+                  className="storyImg"
+                  src="/images/img.boyoonkim/dogimg2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="story_IdTime">
+                <div>sayho_oooo</div>
+                <div>20분 전</div>
+              </div>
+            </div>
+
+            <div className="right_story">
+              <div className="storyAround">
+                <img
+                  className="storyImg"
+                  src="/images/img.boyoonkim/dogimg2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="story_IdTime">
+                <div>sayho_oooo</div>
+                <div>20분 전</div>
+              </div>
+            </div>
+
+            <div className="right_story">
+              <div className="storyAround">
+                <img
+                  className="storyImg"
+                  src="/images/img.boyoonkim/dogimg2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="story_IdTime">
+                <div>sayho_oooo</div>
+                <div>20분 전</div>
+              </div>
+            </div>
+
+            <div className="right_story">
+              <div className="storyAround">
+                <img
+                  className="storyImg"
+                  src="/images/img.boyoonkim/dogimg2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="story_IdTime">
+                <div>sayho_oooo</div>
+                <div>20분 전</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="right_follow">
+            <div className="right_nav">
+              <div>회원님을 위한 추천</div>
+              <div>모두 보기</div>
+            </div>
+
+            <div className="right_recommend">
+              <img
+                className="recommendImg"
+                src="/images/img.boyoonkim/dogimg2.jpg"
+                alt=""
+              />
+              <div className="recommend_IdName">
+                <div>hi990103</div>
+                <div>hyewon님이 팔로우합니다</div>
+              </div>
+              <div className="recommend_follow">팔로우</div>
+            </div>
+
+            <div className="right_recommend">
+              <img
+                className="recommendImg"
+                src="/images/img.boyoonkim/dogimg2.jpg"
+                alt=""
+              />
+              <div className="recommend_IdName">
+                <div>hi990103</div>
+                <div>hyewon님이 팔로우합니다</div>
+              </div>
+              <div className="recommend_follow">팔로우</div>
+            </div>
+
+            <div className="right_recommend">
+              <img
+                className="recommendImg"
+                src="/images/img.boyoonkim/dogimg2.jpg"
+                alt=""
+              />
+              <div className="recommend_IdName">
+                <div>hi990103</div>
+                <div>hyewon님이 팔로우합니다</div>
+              </div>
+              <div className="recommend_follow">팔로우</div>
+            </div>
+
+            <div className="right_recommend">
+              <img
+                className="recommendImg"
+                src="/images/img.boyoonkim/dogimg2.jpg"
+                alt=""
+              />
+              <div className="recommend_IdName">
+                <div>hi990103</div>
+                <div>hyewon님이 팔로우합니다</div>
+              </div>
+              <div className="recommend_follow">팔로우</div>
+            </div>
+
+            <div className="right_recommend">
+              <img
+                className="recommendImg"
+                src="/images/img.boyoonkim/dogimg2.jpg"
+                alt=""
+              />
+              <div className="recommend_IdName">
+                <div>hi990103</div>
+                <div>hyewon님이 팔로우합니다</div>
+              </div>
+              <div className="recommend_follow">팔로우</div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <br />
+      <br />
+      <br />
+    </div>
+  );
 }
 
-export default Mainboyoon;
+export default Main;
