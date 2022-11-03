db87ef735cc66b633f89c907e86619e9a91f6182
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-03 21:08:38 +0900

    ADD: Mission 1-2 구현

--- src/pages/dabinlee/Login/Logindabin.js
+++ src/pages/dabinlee/Login/Logindabin.js
@@ -1,49 +1,51 @@
 import React from 'react';
 import './Logindabin.scss';
 
+function loginBtnOn(e) {
+  console.log('clicked the login btn!!!');
+  e.preventDefault();
+  alert('환영합니다!');
+}
+
 export default function Login() {
+  const idValue = null;
+
   return (
-    <>
-      <div className="login-container">
-        <h1 className="instagram-logo">westagram</h1>
-        <form className="login-box">
-          <input
-            id="id-input"
-            type="text"
-            placeholder="아이디를 입력해주세요"
-            onKeyUp={() => idKeyUp()}
-          />
-          <input
-            type="password"
-            id="password-input"
-            placeholder="비밀번호"
-            onKeyUp={() => pwKeyUp()}
-          />
-          <button
-            // disabled
-            className="button_a login-btn"
-            onSubmit={() => idSubmit()}
-          >
-            로그인하기
-          </button>
-          <h3 className="login-failure_help">비밀번호를 잊으셨나요?</h3>
-        </form>
-      </div>
-    </>
+    <div className="login-container">
+      <h1 className="instagram-logo">westagram</h1>
+      <form
+        className="login-box"
+        onSubmit={event => {
+          event.preventDefault();
+        }}
+      >
+        <input
+          id="id-input"
+          type="text"
+          placeholder="아이디를 입력해주세요"
+          onKeyUp={event => {
+            idvalue = event.target.value;
+          }}
+        />
+        <input
+          type="password"
+          id="password-input"
+          placeholder="비밀번호"
+          onKeyUp={event => {
+            console.log(event);
+          }}
+        />
+        <button
+          // disabled
+          className="button_a login-btn"
+          onClick={() => {
+            console.log('clicked');
+          }}
+        >
+          로그인하기
+        </button>
+        <h3 className="login-failure_help">비밀번호를 잊으셨나요?</h3>
+      </form>
+    </div>
   );
 }
-
-function idKeyUp(event) {
-  console.log('This is ID!');
-}
-
-function pwKeyUp(event) {
-  console.log('This is PW!!');
-}
-
-function idSubmit() {
-  console.log('Want to submit?');
-}
-
-// on+eventListner jsx에 태그에 속성값을(중괄호로 넣고) 으로 주고
-// 밖(어디서든..)에서 함수를 선언 [연결해준다]
