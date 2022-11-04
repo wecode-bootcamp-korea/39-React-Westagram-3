2450ec1e54327fba4e140b7f36a4eb6d7fb46754
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-04 16:45:28 +0900

    ADD:Mission 1-2 SCSS, HTML 수정 완료

--- src/pages/dabinlee/Login/Logindabin.js
+++ src/pages/dabinlee/Login/Logindabin.js
@@ -11,41 +11,43 @@ export default function Login() {
   const idValue = null;
 
   return (
-    <div className="login-container">
-      <h1 className="instagram-logo">westagram</h1>
-      <form
-        className="login-box"
-        onSubmit={event => {
-          event.preventDefault();
-        }}
-      >
-        <input
-          id="id-input"
-          type="text"
-          placeholder="아이디를 입력해주세요"
-          onKeyUp={event => {
-            idvalue = event.target.value;
-          }}
-        />
-        <input
-          type="password"
-          id="password-input"
-          placeholder="비밀번호"
-          onKeyUp={event => {
-            console.log(event);
-          }}
-        />
-        <button
-          // disabled
-          className="button_a login-btn"
-          onClick={() => {
-            console.log('clicked');
+    <div className="body">
+      <div className="login-container">
+        <h1 className="instagram-logo">westagram</h1>
+        <form
+          className="login-box"
+          onSubmit={event => {
+            event.preventDefault();
           }}
         >
-          로그인하기
-        </button>
-        <h3 className="login-failure_help">비밀번호를 잊으셨나요?</h3>
-      </form>
+          <input
+            id="id-input"
+            type="text"
+            placeholder="아이디를 입력해주세요"
+            // onKeyUp={event => {
+            //   idvalue = event.target.value;
+            // }}
+          />
+          <input
+            type="password"
+            id="password-input"
+            placeholder="비밀번호"
+            onKeyUp={event => {
+              console.log(event);
+            }}
+          />
+          <button
+            // disabled
+            className="button_a login-btn"
+            onClick={() => {
+              console.log('clicked');
+            }}
+          >
+            로그인하기
+          </button>
+          <h3 className="login-failure_help">비밀번호를 잊으셨나요?</h3>
+        </form>
+      </div>
     </div>
   );
 }
