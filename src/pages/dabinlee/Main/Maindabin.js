d65aa95908e3e43f97df5b42e93d7820fb1a00c1
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-10 22:06:06 +0900

    Add : Mission 3 - 댓글 기능 구현

--- src/pages/dabinlee/Main/Maindabin.js
+++ src/pages/dabinlee/Main/Maindabin.js
@@ -26,32 +26,85 @@ export default function Maindabin() {
 
   return (
     <>
-      <div className="commentContainer">
-        <img src="/images/img.dabinlee/smile.png" />
-        <input
-          className="commentInput"
-          placeholder="댓글달기..."
-          onChange={saveCoText}
-          type="text"
-          value={coText}
-          onKeyPress={handleKeyDown}
-          //사용자가 키를 눌렀을 때
-        />
-        <button
-          className="commentBtn"
-          onClick={handleClickBtn}
-          disabled={!active}
-        >
-          게시
-        </button>
-      </div>
-      <div className="commentList">
-        <ul>
-          {commentList.map((comment, index) => (
-            <li key={index}> {comment}</li>
-          ))}
-        </ul>
-      </div>
+      <section className="friends_story__container">
+        <div className="friends_story__individual">
+          <div className="friends_story__user1_pic"></div>
+          <h4 className="friends_story__user1_name" />
+          0713.jpg
+        </div>
+        <div className="friends_story__individual">
+          <div className="friends_story__user1_pic"></div>
+          <h4 className="friends_story__user1_name" />
+          0713.jpg
+        </div>
+        <div className="friends_story__individual">
+          <div className="friends_story__user1_pic"></div>
+          <h4 className="friends_story__user1_name" />
+          0713.jpg
+        </div>
+      </section>
+      <section className="feed_container">
+        <div className="feed_indivisual">
+          <div className="feed_user__information">
+            <div className="feed_user__pic" />
+            <h4 className="feed_user__name">0713.jpg</h4>
+          </div>
+          <img
+            className="feed_img"
+            src="/images/img.dabinlee/feedimg.JPG"
+            alt="feed-img"
+          />
+        </div>
+        <section className="feed_container__icons">
+          <img src="/images/img.dabinlee/heart.PNG" alt="heart" />
+          <img src="/images/img.dabinlee/comment.PNG" alt="comment" />
+          <img src="/images/img.dabinlee/message.PNG" alt="message" />
+          <img
+            className="bookmark_icon"
+            src="/images/img.dabinlee/bookmark.PNG"
+            alt="bookmark"
+          />
+        </section>
+        <section className="feed_container__ctx">
+          <h3 className="feed_container__feed_likes">
+            <b>0713.jpg</b>님외 90명이 좋아합니다
+          </h3>
+          <h3 className="feed__ctx">
+            {' '}
+            <b>0713.jpg</b> 봉로포즈... 🌹 💍
+          </h3>
+        </section>
+        <div className="commentList">
+          <ul>
+            {commentList.map((comment, index) => (
+              <li key={index}>
+                {' '}
+                <span>0713.jpg </span>
+                {comment}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="comment_container">
+          <img src="/images/img.dabinlee/smile.png" />
+          <input
+            className="commentInput"
+            placeholder="댓글달기..."
+            onChange={saveCoText}
+            type="text"
+            value={coText}
+            onKeyPress={handleKeyDown}
+            //사용자가 키를 눌렀을 때
+          />
+          <button
+            className="commentBtn"
+            onClick={handleClickBtn}
+            disabled={!active}
+          >
+            게시
+          </button>
+        </div>
+      </section>
     </>
   );
 }
