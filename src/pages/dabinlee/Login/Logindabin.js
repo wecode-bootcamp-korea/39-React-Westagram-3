b03efb903d0f17d3a28ce7887d3aff91cfeb4fe1
Da-bin, Lee <110847597+Dabnii@users.noreply.github.com>
2022-11-12 20:01:06 +0900

    Modified: 계산된 속성 적용

--- src/pages/dabinlee/Login/Logindabin.js
+++ src/pages/dabinlee/Login/Logindabin.js
@@ -3,75 +3,69 @@ import { useNavigate } from 'react-router-dom';
 import './Logindabin.scss';
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  const saveUserId = event => {
-    setUsername(event.target.value);
-  };
-  const savePwValue = event => {
-    setPassword(event.target.value);
+  const [inputValue, setInputValue] = useState({
+    id: '',
+    pw: '',
+  });
+
+  const { id, pw } = inputValue;
+  const isValid = id.includes('@') && pw.length >= 5;
+
+  const handleUserInfo = e => {
+    const { name, value } = e.target;
+    setInputValue(prev => ({ ...prev, [name]: value }));
   };
 
-  const isValid = username.indexOf('@') !== -1 && password.length >= 5;
-
-  const loginSucess = () => {
-    isValid ? navigate('/main-dabin') : alert('비밀번호를 확인해주세요!');
-  };
-
-  const pressEnter = e => {
-    if (isValid === true && e.keyCode === 13) {
-      fetch('http://10.58.52.220:3000/auth/signin', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: username,
-          password: password,
-        }),
+  const signIn = () => {
+    fetch('http://10.58.52.220:3000/auth/signin', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: inputValue.id,
+        password: inputValue.pw,
+      }),
+    })
+      .then(response => {
+        if (response.ok === true) {
+          return response.json();
+        }
+        throw new Error('로그인 실패');
       })
-        .then(response => {
-          if (response.ok === true) {
-            return response.json();
-          }
-          throw new Error('로그인 실패');
-        })
-        .catch(error => alert('로그인 실패'))
-        .then(data => {
-          localStorage.setItem('TOKEN', data.accessToken);
-          alert('로그인 성공');
-          navigate('/main-dabin');
-        });
-    }
+      .catch(error => alert('비밀번호를 확인해주세요!'))
+      .then(data => {
+        localStorage.setItem('TOKEN', data.accessToken);
+        alert('로그인 성공');
+        navigate('/main-dabin');
+      });
   };
-
-  const disable = isValid ? false : true;
 
   return (
-    <div className="body">
-      <div className="login-container">
-        <h1 className="instagram-logo">westagram</h1>
-        <div className="login-box">
+    <div className="Logindabin">
+      <div className="loginContainer">
+        <h1 className="instagramLogo">westagram</h1>
+        <div className="loginBox">
           <input
-            id="id-input"
+            className="idInput"
+            name="id"
             type="text"
             placeholder="아이디를 입력해주세요"
-            onChange={saveUserId}
-            onKeyDown={pressEnter}
+            onChange={handleUserInfo}
           />
           <input
+            className="passwordInput"
+            name="pw"
             type="password"
-            id="password-input"
             placeholder="비밀번호"
-            onChange={savePwValue}
-            onKeyDown={pressEnter}
+            onChange={handleUserInfo}
           />
-          <button disabled={disable} className="loginBtn" onClick={loginSucess}>
+          <button disabled={!isValid} className="loginBtn" onClick={signIn}>
             로그인하기
           </button>
-          <h3 className="login-failure_help">비밀번호를 잊으셨나요?</h3>
+          <h3 className="loginFailureHelp">비밀번호를 잊으셨나요?</h3>
         </div>
       </div>
     </div>
